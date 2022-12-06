3a927fddf44ef2674f93990f12fdce0d5692aa1c
Janeth Pérez <janeth_2124@hotmail.com>
2022-12-06 16:01:41 -0600

    api con axiox
    se consume api con axiox y se carga de manera dinamica las ordenes a preparar por el chef

---
+++ burger-queen9/src/routes/authRoutes/chef/resumOrder.js
@@ -0,0 +1,18 @@
+export const ResumOrdns = () => {
+    return (
+        <>
+         <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Orden:
+                                    <p className='text-end'>#</p>
+                                </h5>
+                                <p className="card-text">Hora de entrada: 00:00 hrs.</p>
+                                <span>Timer: 00:00 hrs </span>
+                                <div className='text-end'>
+                                    <a href="#" className="btn btn-primary">Preparar</a>
+                                </div>
+                            </div>
+                        </div>
+        </>
+    )
+}
